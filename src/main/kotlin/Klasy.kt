af4cb094f349e50053f2927d99ae2eee646cc8ba
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-10 21:14:55 +0200

    dodanie Booli do kolorow flagi

--- src/main/kotlin/Klasy.kt
+++ src/main/kotlin/Klasy.kt
@@ -1,6 +1,10 @@
-class Panstwo(n: String,k: Int, u: String, z: Boolean){
+class Panstwo(n: String,k: Int, u: String, z: Boolean, cze: Boolean, bi: Boolean, ni: Boolean, cza: Boolean){
     var nazwa = n
     var kolory = k
     var ulozenie = u
     var znaczek = z
+    var czerwony = cze
+    var bialy = bi
+    var niebieski = ni
+    var czarny = cza
 }
